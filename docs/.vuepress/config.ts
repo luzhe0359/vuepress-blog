10b4b62bfecf0b36c2c0e36a3ce12233e99316a7
luzhe0359 <luzhe0359@163.com>
2022-08-08 09:57:14 +0800

    🙃移除vuepress-blog base

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -8,20 +8,10 @@ export default defineUserConfig({
   title: "足各路的小仓库",
   description: "一个收藏了各种奇珍异宝的小仓库",
 
-  base: "/vuepress-blog/",
-
-  // plugins,
-
   theme,
 
   alias: {
     "@LinkList": path.resolve(__dirname, "components/LinkList.vue"),
-    // 你可以在这里将别名定向到自己的组件
-    // 比如这里我们将主题的主页组件改为用户 .vuepress/components 下的 HomePage.vue
-    // "@theme-hope/components/HomeFeatures": path.resolve(
-    //   __dirname,
-    //   "./components/HomePage.vue"
-    // ),
   },
 
 });
